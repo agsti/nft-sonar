bdd750f6b84b8ff5d0ef2c483d824e9533116f76
Agusti Bau <agustibau@gmail.com>
2022-01-05 14:15:15 +0100

    Moved backend to backend folder

--- backend/main/etl.py
+++ backend/main/etl.py
@@ -5,9 +5,13 @@ from etl.hash_images import hash_images
 
 
 def main():
+    print("=" * 20, " FETCHING COLLECTIONS ", "=" * 20)
     fetch_collections(1)
+    print("=" * 23, " FETCHING ASSETS ", "=" * 20)
     get_and_save_all_assets()
+    print("=" * 20, " DOWNLOADING PICTURES ", "=" * 20)
     download_all_pictures()
+    print("=" * 20, " HASHING IMAGES ", "=" * 20)
     hash_images()
 
 
